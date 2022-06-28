92d6e2af29af7935788ddc82461bfb618a68401b
hrshuv0 <h.r.shuvo9@gmail.com>
2022-06-28 08:16:44 +0600

    added notice view

--- Assignment/Controllers/HomeController.cs
+++ Assignment/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using Assignment.Data;
 using Microsoft.AspNetCore.Mvc;
 using Assignment.Models;
 
@@ -7,15 +8,21 @@ namespace Assignment.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _dbContext;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext dbContext)
     {
         _logger = logger;
+        _dbContext = dbContext;
     }
 
     public IActionResult Index()
     {
-        return View();
+        var notice = _dbContext.Notices!
+            .OrderByDescending(n => n.Id)
+            .ToList();
+        
+        return View(notice);
     }
 
     public IActionResult Privacy()
